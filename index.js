1df5849cddcb62c7844d3033847199a900b6c1c2
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-04-09 18:49:18 +0200

    Created the necessary const for the js part

---
+++ index.js
@@ -0,0 +1,4 @@
+const submitButton = document.querySelector('.submit-btn');
+const ratingText = document.querySelector('.the-rating');
+const selectedRating = document.querySelector('input[name="rating"]:checked').value;
+
